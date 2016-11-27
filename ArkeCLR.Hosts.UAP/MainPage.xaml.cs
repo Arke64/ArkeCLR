87f974cd30b28460867405d4c4d6a310bb668a65
John Brochue <johnbrochue@outlook.com>
2016-11-27 15:40:59 -0500

    Added empty project skeletons.

---
+++ ArkeCLR.Hosts.UAP/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace ArkeCLR.Hosts.UAP {
+    public sealed partial class MainPage : Page {
+        public MainPage() {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ ArkeCLR.Hosts.UAP/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArkeCLR.Hosts.UAP")]
+[assembly: AssemblyProduct("ArkeCLR")]
+[assembly: AssemblyCopyright("Copyright © John Brochue 2016")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
