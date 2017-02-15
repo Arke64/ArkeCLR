00c05d309a70f2a179906bc2f504e21a80f67367
John Brochue <johnbrochue@outlook.com>
2017-02-14 19:52:15 -0500

    Deleted Metadata project.

--- ArkeCLR.Metadata/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("ArkeCLR.Metadata")]
-[assembly: AssemblyProduct("ArkeCLR")]
-[assembly: AssemblyCopyright("Copyright © John Brochue 2016")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
