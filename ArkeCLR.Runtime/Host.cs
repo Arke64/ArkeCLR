35969d68f75b3b046a231512653a451191f1d64e
John Brochue <johnbrochue@outlook.com>
2017-02-25 20:44:05 -0500

    Print out the rest of the tables.

--- ArkeCLR.Runtime/Host.cs
+++ ArkeCLR.Runtime/Host.cs
@@ -27,8 +27,43 @@ namespace ArkeCLR.Runtime {
             this.entryAssembly.GuidStream.ReadAll().ForEach(s => Console.WriteLine(s));
 
             this.entryAssembly.TableStream.Modules.ForEach(t => Console.WriteLine(t));
-            this.entryAssembly.TableStream.TypeDefs.ForEach(t => Console.WriteLine(t));
             this.entryAssembly.TableStream.TypeRefs.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.TypeDefs.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.Fields.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.MethodDefs.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.Params.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.InterfaceImpls.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.MemberRefs.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.Constants.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.CustomAttributes.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.FieldMarshals.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.DeclSecurities.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.ClassLayouts.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.FieldLayouts.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.StandAloneSigs.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.EventMaps.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.Events.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.PropertyMaps.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.Properties.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.MethodSemantics.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.MethodImpls.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.ModuleRefs.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.TypeSpecs.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.ImplMaps.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.FieldRVAs.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.Assemblies.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.AssemblyProcessors.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.AssemblyOSs.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.AssemblyRefs.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.AssemblyRefProcessors.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.AssemblyRefOSs.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.Files.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.ExportedTypes.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.ManifestResources.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.NestedClasses.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.GenericParams.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.MethodSpecs.ForEach(t => Console.WriteLine(t));
+            this.entryAssembly.TableStream.GenericParamConstraints.ForEach(t => Console.WriteLine(t));
 
             return 0;
         }
