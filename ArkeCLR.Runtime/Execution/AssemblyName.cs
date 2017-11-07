d7726572b0b8d700696c08385a80b5114d69581d
John Brochue <johnbrochue@outlook.com>
2017-11-06 20:20:20 -0500

    Made public key token a byte array in AssemblyName.

--- ArkeCLR.Runtime/Execution/AssemblyName.cs
+++ ArkeCLR.Runtime/Execution/AssemblyName.cs
@@ -1,23 +1,27 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 
 namespace ArkeCLR.Runtime.Execution {
     public struct AssemblyName {
         public string Name { get; }
         public Version Version { get; }
         public CultureInfo Culture { get; }
-        public string PublicKeyToken { get; }
+        public byte[] PublicKeyToken { get; }
         public string HintPath { get; }
 
         public AssemblyName(string name) : this(name, default) { }
         public AssemblyName(string name, string hintPath) : this(name, default, default(string), default, hintPath) { }
-        public AssemblyName(string name, string version, string culture, string publicKeyToken) : this(name, version, culture, publicKeyToken, default) { }
-        public AssemblyName(string name, string version, string culture, string publicKeyToken, string hintPath) : this(name, version != null ? Version.Parse(version) : new Version(0, 0, 0, 0), culture != null ? new CultureInfo(culture) : CultureInfo.InvariantCulture, publicKeyToken, hintPath) { }
-        public AssemblyName(string name, Version version, CultureInfo culture, string publicKeyToken, string hintPath) => (this.Name, this.Version, this.Culture, this.PublicKeyToken, this.HintPath) = (name, version, culture, publicKeyToken, hintPath);
+        public AssemblyName(string name, string version, string culture, byte[] publicKeyToken) : this(name, version, culture, publicKeyToken, default) { }
+        public AssemblyName(string name, string version, string culture, byte[] publicKeyToken, string hintPath) : this(name, version != null ? Version.Parse(version) : new Version(0, 0, 0, 0), culture != null ? new CultureInfo(culture) : CultureInfo.InvariantCulture, publicKeyToken, hintPath) { }
+        public AssemblyName(string name, Version version, CultureInfo culture, byte[] publicKeyToken) : this(name, version, culture, publicKeyToken, default) { }
+        public AssemblyName(string name, Version version, CultureInfo culture, byte[] publicKeyToken, string hintPath) => (this.Name, this.Version, this.Culture, this.PublicKeyToken, this.HintPath) = (name, version, culture, publicKeyToken, hintPath);
 
         public static AssemblyName FromFilePath(string filePath) => new AssemblyName(Path.GetFileNameWithoutExtension(filePath), filePath);
 
-        public string FullName => $"{this.Name}, Version={this.Version}, Culture={this.Culture.Name}, PublicKeyToken={(this.PublicKeyToken ?? "null")}";
+        public string FullName => $"{this.Name}, Version={this.Version}, Culture={this.Culture.Name}, PublicKeyToken={(AssemblyName.FormatPublicKeyToken(this.PublicKeyToken))}";
+
+        private static string FormatPublicKeyToken(byte[] token) => token != null ? string.Join(string.Empty, token.Select(b => b.ToString("X2"))) : new string('0', 16);
     }
 }
