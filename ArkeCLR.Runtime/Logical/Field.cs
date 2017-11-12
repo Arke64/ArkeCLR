fcf0fdad6268f39566d937e6c28712af330fa278
John Brochue <johnbrochue@outlook.com>
2017-11-12 13:40:40 -0500

    Added logical field.

---
+++ ArkeCLR.Runtime/Logical/Field.cs
@@ -0,0 +1,15 @@
+﻿using ArkeCLR.Runtime.Files;
+
+namespace ArkeCLR.Runtime.Logical {
+    public class Field {
+        public Type Type { get; }
+        public uint Row { get; }
+        public string Name { get; }
+
+        public Field(CliFile file, Type type, Tables.Field def, uint row) {
+            this.Type = type;
+            this.Row = row;
+            this.Name = file.StringStream.GetAt(def.Name);
+        }
+    }
+}
--- ArkeCLR.Runtime/Logical/Type.cs
+++ ArkeCLR.Runtime/Logical/Type.cs
@@ -9,6 +9,7 @@ namespace ArkeCLR.Runtime.Logical {
         public string Name { get; }
         public string Namespace { get; }
         public IReadOnlyCollection<Method> Methods { get; }
+        public IReadOnlyCollection<Field> Fields { get; }
 
         public Type(CliFile file, Assembly assembly, TypeDef def, uint row) {
             this.Assembly = assembly;
@@ -16,6 +17,7 @@ namespace ArkeCLR.Runtime.Logical {
             this.Name = file.StringStream.GetAt(def.TypeName);
             this.Namespace = file.StringStream.GetAt(def.TypeNamespace);
             this.Methods = file.TableStream.MethodDefs.ExtractRun(file.TableStream.TypeDefs, p => p.MethodList.Row, def, row, (d, r) => new Method(file, this, d, r));
+            this.Fields = file.TableStream.Fields.ExtractRun(file.TableStream.TypeDefs, p => p.FieldList.Row, def, row, (d, r) => new Field(file, this, d, r));
         }
     }
 }
