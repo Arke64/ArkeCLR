2b1a00a2b22ea6cb469eddcf4ca1a737f5271316
John Brochue <johnbrochue@outlook.com>
2017-11-12 02:00:04 -0500

    Changed file resolver to take a list of search directories.

--- ArkeCLR.Hosts.Console/FileResolver.cs
+++ ArkeCLR.Hosts.Console/FileResolver.cs
@@ -3,32 +3,36 @@ using System.IO;
 
 namespace ArkeCLR.Hosts.Console {
     public class FileResolver : IFileResolver {
-        private readonly string currentDirectory;
+        private readonly string[] searchDirectories;
 
-        public FileResolver(string currentDirectory) => this.currentDirectory = currentDirectory;
+        public FileResolver(params string[] searchDirectories) => this.searchDirectories = searchDirectories;
 
         public bool TryResolve(string fileName, string hintPath, out byte[] result) {
-            var path = default(string);
-            var root = Path.Combine(this.currentDirectory, fileName);
-
             result = default;
 
-            if (File.Exists(hintPath)) {
-                path = hintPath;
-            }
-            else if (File.Exists(root + ".dll")) {
-                path = root + ".dll";
-            }
-            else if (File.Exists(root + ".exe")) {
-                path = root + ".exe";
-            }
-            else {
-                return false;
+            foreach (var d in this.searchDirectories) {
+                var path = default(string);
+                var root = Path.Combine(d, fileName);
+
+                if (File.Exists(hintPath)) {
+                    path = hintPath;
+                }
+                else if (File.Exists(root + ".dll")) {
+                    path = root + ".dll";
+                }
+                else if (File.Exists(root + ".exe")) {
+                    path = root + ".exe";
+                }
+                else {
+                    continue;
+                }
+
+                result = File.ReadAllBytes(path);
+
+                return true;
             }
 
-            result = File.ReadAllBytes(path);
-
-            return true;
+            return false;
         }
     }
 }
